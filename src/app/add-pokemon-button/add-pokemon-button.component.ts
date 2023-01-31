9e464dbae181e0504a3d87a5bc0a5c1fe4f7ed3f
Jaypal Mudaliyar <jaypal.mudaliyar24@gmail.com>
2023-01-31 16:01:06 +0530

    ENH: Created a Modal Button Component, Error while adding new pokemon

--- src/app/add-pokemon-button/add-pokemon-button.component.ts
+++ src/app/add-pokemon-button/add-pokemon-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import {
   FormBuilder,
   FormControl,
@@ -7,11 +7,88 @@ import {
 } from '@angular/forms';
 import { PokemonModel } from '.././model/pokemon.model';
 import { PokemonService } from '.././services/pokemon.service';
-import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-add-pokemon-button',
   templateUrl: './add-pokemon-button.component.html',
   styleUrls: ['./add-pokemon-button.component.css'],
 })
-export class AddPokemonButtonComponent {}
+export class AddPokemonButtonComponent implements OnInit {
+  @Input() allPokemon: PokemonModel[] = [];
+  @Input() refreshPokemons: Function = () => {};
+  @Input() setErrorMessage: Function = () => {};
+  pokemonForm: FormGroup;
+
+  validUrl = '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?';
+
+  constructor(
+    private fb: FormBuilder,
+    private pokemonService: PokemonService,
+    private modalService: NgbModal
+  ) {
+    this.pokemonForm = fb.group({});
+  }
+
+  open(content: any) {
+    this.modalService
+      .open(content, { ariaLabelledBy: 'modal-basic-title' })
+  }
+
+  ngOnInit(): void {
+    this.pokemonForm = this.fb.group({
+      name: ['', [Validators.required, Validators.pattern(/[A-Za-z]\d*$/)]],
+
+      type: ['', [Validators.required]],
+
+      imgUrl: ['', [Validators.required, Validators.pattern(this.validUrl)]],
+    });
+  }
+
+  public get Name(): FormControl {
+    return this.pokemonForm.get('name') as FormControl;
+  }
+
+  public get Types(): FormControl {
+    return this.pokemonForm.get('type') as FormControl;
+  }
+
+  public get ImageUrl(): FormControl {
+    return this.pokemonForm.get('imgUrl') as FormControl;
+  }
+
+  clearForm() {
+    this.Name.reset();
+    this.Types.reset();
+    this.ImageUrl.reset();
+  }
+  addPokemon() {
+    let pokemon: PokemonModel = {
+      name: this.Name.value,
+      type: this.Types.value,
+      imgUrl: this.ImageUrl.value,
+    };
+
+    this.pokemonService.addPokemon(pokemon).subscribe({
+      next: (response) => {
+        this.allPokemon.unshift(response);
+        this.refreshPokemons();
+        this.clearForm();
+      },
+      error: (err) => {
+        this.setErrorMessage(err.message);
+      },
+    });
+  }
+
+  saveChanges(modal: any) {
+    modal.close('Save click');
+    this.addPokemon();
+  }
+
+  closeModal(modal: any) {
+    modal.close('Save click');
+    this.clearForm();
+  }
+}
+
--- src/app/pokemon-show/pokemon-show.component.ts
+++ src/app/pokemon-show/pokemon-show.component.ts
@@ -1,13 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  Validators,
-} from '@angular/forms';
 import { PokemonModel } from '.././model/pokemon.model';
 import { PokemonService } from '.././services/pokemon.service';
-import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-pokemon-show',
@@ -15,123 +8,62 @@ import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./pokemon-show.component.css'],
 })
 export class PokemonShowComponent implements OnInit {
-  pokemonForm: FormGroup;
   allPokemon: PokemonModel[];
   pokemonsOnThisPage: PokemonModel[];
   isLoaded: boolean;
   errMsg: string;
-  pokemon: PokemonModel = new PokemonModel();
 
   validUrl = '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?';
-  closeResult = '';
 
   page = 1;
   pageSize = 8;
   collectionSize: number = 0;
 
   constructor(
-    private fb: FormBuilder,
     private pokemonService: PokemonService,
-    private modalService: NgbModal
   ) {
-    this.pokemonForm = fb.group({});
     this.allPokemon = [];
     this.pokemonsOnThisPage = [];
     this.isLoaded = false;
     this.errMsg = '';
   }
 
-  open(content: any) {
-    this.modalService
-      .open(content, { ariaLabelledBy: 'modal-basic-title' })
-      .result.then(
-        (result) => {
-          this.closeResult = `Closed with: ${result}`;
-        },
-        (reason) => {
-          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-        }
-      );
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
-  }
-
   ngOnInit(): void {
-    this.pokemonForm = this.fb.group({
-      name: ['', [Validators.required, Validators.pattern(/[A-Za-z]\d*$/)]],
-
-      type: ['', [Validators.required]],
-
-      imgUrl: ['', [Validators.required, Validators.pattern(this.validUrl)]],
-    });
     this.pokemonService.getPokemon().subscribe({
       next: (response) => {
         for (let pokemon of response) {
           this.allPokemon.unshift(pokemon);
         }
-        this.collectionSize = this.allPokemon.length;
         this.refreshPokemons();
       },
       error: (err) => {
-        this.errMsg = err.message;
+        this.setErrorMessage(err.message);
       },
     });
-  }
-
-  public get Name(): FormControl {
-    return this.pokemonForm.get('name') as FormControl;
-  }
-
-  public get Types(): FormControl {
-    return this.pokemonForm.get('type') as FormControl;
-  }
-
-  public get ImageUrl(): FormControl {
-    return this.pokemonForm.get('imgUrl') as FormControl;
-  }
-
-  clearForm() {
-    this.Name.setValue('');
-    this.Types.setValue('');
-    this.ImageUrl.setValue('');
   }
 
   setIsloaded() {
     this.isLoaded = !this.isLoaded;
   }
-  setCloseErr() {
-    this.errMsg = '';
-  }
-  addPokemon() {
-    let pokemon: PokemonModel = {
-      name: this.Name.value,
-      type: this.Types.value,
-      imgUrl: this.ImageUrl.value,
-    };
-
-    this.pokemonService.addPokemon(pokemon).subscribe({
-      next: (response) => {
-        this.allPokemon.unshift(response);
-        this.clearForm();
-      },
-      error: (err) => {
-        this.errMsg = err.message;
-      },
-    });
+  setErrorMessage(message: string) {
+    this.errMsg = message;
   }
 
   refreshPokemons() {
+    console.log("Function Called");
+    this.allPokemon.unshift({
+      name: "ABC",
+      type: "ABC",
+      imgUrl: "ABC",
+    });
+    this.collectionSize = this.allPokemon.length;
+    console.log(this.allPokemon);
+    console.log(this.page, this.pageSize); 
+    
     this.pokemonsOnThisPage = this.allPokemon.slice(
       (this.page - 1) * this.pageSize,
       (this.page - 1) * this.pageSize + this.pageSize
     );
+    
   }
 }
